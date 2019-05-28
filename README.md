fc8e0c0ca016f0e16017029340d980b53fb648c7
Denis Slepov <tmnkun@gmail.com>
2019-05-29 00:18:41 +0500

    Change to right install.sh, add README

---
+++ README.md
@@ -0,0 +1,4 @@
+Tested by Ubuntu 18.04. 
+
+
+If you use Ubuntu 19.04 change ```$dockerrelease``` varialabel to *test*.
--- install.sh
+++ install.sh
@@ -1,37 +1,76 @@
----
+#!/bin/bash
 
-- name: run the playbook tasks on the localhost configuration
-  hosts: localhost
-  connection: local
-  become: yes
-  vars:
-    home_dir: ${HOME}
-    packages:
-    - build-essential
-    - nodejs
-    - golang
-    - python-dev
-    - libreadline-dev
-    - libbz2-dev
-    - libssl-dev
-    - libsqlite3-dev
-    - libxslt1-dev
-    - libxml2-dev
-    - libffi-dev
+# Install ssh
+echo -e "\n\n\n" | ssh-keygen -t rsa -b 4096 -C "tmnkun@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+getidrsapub=$(cat "$HOME"/.ssh/id_rsa.pub)
 
-  tasks:
 
-  - name: Install a list of packages
-    apt:
-      name: "{{ packages }}"
+# Install prerequisit
+sudo apt update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common \
+    fonts-firacode \
+    shellcheck \
+    git
 
-  - name: Configuration bashrc
-    blockinfile:
-      path: "{{ home_dir }}/.bashrc"
-      block: |
-        export PATH="$HOME/.pyenv/bin:$PATH"
-        eval "$(pyenv init -)"
-        eval "$(pyenv virtualenv-init -)"
-        GIT_PROMPT_ONLY_IN_REPO=1
-        source ~/.bash-git-prompt/gitprompt.sh
-      backup: yes
+git config --global user.name "Denis Slepov"
+git config --global user.email "tmnkun@gmail.com"
+
+git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1
+mv .bash-git-prompt "${HOME}"
+
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+
+
+# Install Ansible
+getansibleversion=$(ansible --version)
+if [ "ansible" == "${getansibleversion:0:7}" ]; then
+    echo "${getansibleversion:0:13} is install"
+else
+    sudo apt-add-repository --yes --update ppa:ansible/ansible && sudo apt install -y ansible
+fi
+
+
+# Install Docker
+getdockerversion=$(docker -v)
+dockerrelease="stable"
+if [ "Docker" == "${getdockerversion:0:6}" ]; then
+    echo "${getansibleversion:0} is install"
+else
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    ${dockerrelease}"
+    sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+    sudo usermod -aG docker "${USER}"
+fi
+
+
+# Install Google Chrome
+sudo apt-get install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome*.deb
+
+
+# Install software using snap
+sudo snap install code --classic
+sudo snap install telegram-desktop
+sudo snap install micro --classic
+
+
+# Run ansible playbook
+sudo ansible-playbook -K install.yml
+
+
+# Postinstall tasks
+source "${HOME}"/.bashrc
+echo "Enter public key to you github $getidrsapub"
+rm -f google-chrome*.deb 
+su "${USER}"
